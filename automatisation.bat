92a77de1eb20c49f2ffa185bd46248ed06dd26e4
21902991 <21902991@etu.unicaen.fr>
2020-11-13 14:19:49 +0100

    Premier commit import des fichiers initiaux

---
+++ automatisation.bat
@@ -0,0 +1,7 @@
+SET MAVEN_HOME=C:\Users\leo22\Documents\apache-maven-3.6.3
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_241
+
+SET PATH=%MAVEN_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+
+mvn cobertura:cobertura jar:jar
+
